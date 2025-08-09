124ba1f7d58d40dbe91f04f1fca4655686ee3477
Rafael Augusto de Souza Silva <rafaelaugusto335@gmail.com>
2025-08-09 01:38:31 -0300

    feat: add sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+import Header from "@/components/common/header";
 
 const Home = () => {
   return (
     <div>
-      <Button>Bootcamp</Button>
-      <h1>Home</h1>
+      <Header />
     </div>
   );
 };
